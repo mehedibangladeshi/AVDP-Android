50e679aba51041081e3c81541af2a9f5e1239062
Mehedi <mehedipy@gmail.com>
2019-11-17 05:37:40 +0600

    init commit

---
+++ app/src/main/java/com/mehedihasanbangladeshi/allvariantdp/SampleActivity.java
@@ -0,0 +1,17 @@
+package com.mehedihasanbangladeshi.allvariantdp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.avdp_sample);
+//        setContentView(R.layout.dp_sample);
+
+
+    }
+}
